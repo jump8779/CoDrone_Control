22dc88e7f1b2898dbc49b7cf5612d4192065abb7
jump8779 <jump154159@gmail.com>
2019-09-27 01:05:07 +0900

    WindowsForm 내부에서 Python 코드 테스트
    + Bt_DroneStart 동작시 출력 테스트
    
    <=====>
    
    각 python 내의 def들을 windowsForm 기능별로 분리해서 적용시켜 줄 방법 구상
    + 각 동작마다 CreateEngine을 실행
    + Start 동작 내에서 각 기능들 Invoke로 총괄 관리
     ㅡ> 결국 함수 정의를 하려면 같은 문제 발생

--- DroneControl/DroneControl/Form1.cs
+++ DroneControl/DroneControl/Form1.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
+using IronPython;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using IronPython.Modules;
+
 namespace DroneControl
 {
     public partial class Main : Form
@@ -29,7 +34,38 @@ namespace DroneControl
         }
         private void Bt_DroneStart_Click(object sender, EventArgs e)
         {
+            var engine = IronPython.Hosting.Python.CreateEngine();
 
+            var scope = engine.CreateScope();
+
+            try
+            {
+                var source = engine.CreateScriptSourceFromFile("pyCoDrone.py");
+                source.Execute(scope);
+
+                var Fnhelloworld = scope.GetVariable<Func<object>>("HelloWorld");
+                rTB_position.Text += Fnhelloworld() +"\r\n";
+
+                var Fnhelloworld2 = scope.GetVariable<Func<object, object>>("HelloWorld2");
+                rTB_position.Text += Fnhelloworld2("HelloWorld2") + "\r\n";
+
+                var FnListTest = scope.GetVariable<Func<object>>("ListTest");
+
+                List r = (List)FnListTest();
+                foreach (string data in r)
+                {
+                    rTB_position.Text += "result:" + data + "\r\n";
+                }
+
+                var myClass = scope.GetVariable<Func<object, object>>("MyClass");
+                var myInstance = myClass("world");
+
+                rTB_position.Text += engine.Operations.GetMember(myInstance, "value");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void Bt_EmergencyStop_Click(object sender, EventArgs e)
---
+++ DroneControl/DroneControl/bin/Debug/pyCoDrone.py
@@ -0,0 +1,18 @@
+def HelloWorld():
+    data = 'Hello World C#'
+    return data
+  
+def HelloWorld2( data ):
+    return data
+ 
+def ListTest():
+ 
+    data = []
+    data.append('Hello')
+    data.append('World')
+    data.append('black falcon')
+    return data
+ 
+class MyClass(object):
+    def __init__(self, value):
+        self.value = value
