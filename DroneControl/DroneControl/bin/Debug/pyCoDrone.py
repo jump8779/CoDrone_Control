555e04fd1b2cdbea78de6a55dc278a451475f6a0
jump8779 <jump154159@gmail.com>
2019-09-29 02:57:25 +0900

    코드론의 호버링과 이동 경로 값 추적
    set_plot_sensor() 함수를 이용하여 PlotType.gyro 값을 그래프로 그려내었을 때
    get_trim()으로 받아온 gyro값은 0임에도 불구하고 그래프 값에 변동이 있는점,
    실제 드론의 호버링 도중 가만히 있지 못하고 흘러다녔던 점에서 착안하여
    set_plot_sensor() 함수가 plot하는 리스트 데이터를 분석해 해당 값이
    Data 클래스의 eventUpdateImu 내의 gyro값이라 판단, 테스트
    
    => set_plot_sensor() 함수가 있을때에만 값에 변화가 생기며, 값이 일관성 없이 변화가 크다
    
    ====================================
    
    기존 Android 코드론 라이브러리에 드론 자세 정보를 출력해주는 함수 Request_DroneAttitude()
    에서 착안해 이번엔 Data 클래스 내의 attitude를 사용해 테스트
    
    => set_plot_sensor() 함수가 없어도 잘 동작 하며 값이 얼추 맞게 나오지만,
    호버링을 위한 미세조정을 하기엔 값의 정밀도가 떨어진다. 같은 의미로
    이동 거리값을 측정하기에도 정밀도가 떨어진다.

--- DroneControl/DroneControl/bin/Debug/pyCoDrone.py
+++ DroneControl/DroneControl/bin/Debug/pyCoDrone.py
@@ -1,18 +1,36 @@
-def HelloWorld():
-    data = 'Hello World C#'
-    return data
-  
-def HelloWorld2( data ):
-    return data
- 
-def ListTest():
- 
-    data = []
-    data.append('Hello')
-    data.append('World')
-    data.append('black falcon')
-    return data
- 
-class MyClass(object):
-    def __init__(self, value):
-        self.value = value
+import CoDrone
+from CoDrone.protocol import *
+import threading
+import time
+
+drone = CoDrone.CoDrone()
+
+def thgyro():
+   R = 0
+   P = 0
+   while(1):
+      R += drone._data.attitude.ROLL
+      P += drone._data.attitude.PITCH
+      print("ROLL:", drone._data.attitude.ROLL)
+      print("PITCH:", drone._data.attitude.PITCH)
+      print("YAW:", drone._data.attitude.YAW)
+      time.sleep(1)
+   print("R:", R)
+   print("P:", P)
+
+def main():
+   t1 = threading.Thread(target=thgyro)
+   t1.daemon = True
+   drone.pair()
+   battery = drone.get_battery_percentage()
+   print("battery:", battery)
+   drone.takeoff()
+   t1.start()
+   drone.hover(3)
+   drone.set_pitch(100)
+   drone.move(5)
+   drone.land()
+   drone.disconnect()
+
+if __name__ == '__main__':
+   main()
