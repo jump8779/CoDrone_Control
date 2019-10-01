c09123809bec4ef9a9fe40f758f056732a1ff9e1
jump8779 <jump154159@gmail.com>
2019-10-01 18:17:17 +0900

    GUI 동작 구현 (2)
    + 별도의 Thread를 생성하지 않고 GUI mainloop 내에서 생성하도록 구현
    + 드론 연결동작시 값의 업데이트 루프가 멈춰버리는 현상

--- DroneControl/DroneControl/bin/Debug/pyCoDrone.py
+++ DroneControl/DroneControl/bin/Debug/pyCoDrone.py
@@ -7,54 +7,6 @@ from threading import *
 drone = CoDrone()
 
 Flag = True
-battery_per = 0
-temp = 0
-press = 0
-attitude_R = 0
-attitude_P = 0
-attitude_Y = 0
-angle_R = 0
-angle_P = 0
-angle_Y = 0
-acc_X = 0
-acc_Y = 0
-acc_Z = 0
-
-def droneStat():
-   global temp
-   global press
-   global battery_per
-   while(drone.isConnected()):
-      temp = drone.get_drone_temp()
-      press = drone.get_pressure()
-      battery_per = drone.get_battery_percentage()
-
-def droneAtti():
-   global attitude_R 
-   global attitude_P 
-   global attitude_Y
-   while(drone.isConnected()):
-      attitude_R = drone._data.attitude.ROLL
-      attitude_P = drone._data.attitude.PITCH
-      attitude_Y = drone._data.attitude.YAW
-
-def droneAngl():
-   global angle_R 
-   global angle_P 
-   global angle_Y 
-   while(drone.isConnected()):
-      angle_R = drone._data.gyro.ROLL
-      angle_P = drone._data.gyro.PITCH
-      angle_Y = drone._data.gyro.YAW
-
-def droneAcce():
-   global acc_X 
-   global acc_Y 
-   global acc_Z 
-   while(drone.isConnected()):
-      acc_X = drone._data.accel.X
-      acc_Y = drone._data.accel.Y
-      acc_Z = drone._data.accel.Z
 
 def connect():
    drone.pair()
@@ -113,6 +65,18 @@ class MyFrame(Frame):
       self.master = master
       self.master.title("Drone_Co")
       self.pack(fill=BOTH, expand=True)
+      self.battery_per = 0
+      self.temp = 0
+      self.press = 0
+      self.attitude_R = 0
+      self.attitude_P = 0
+      self.attitude_Y = 0
+      self.angle_R = 0
+      self.angle_P = 0
+      self.angle_Y = 0
+      self.acc_X = 0
+      self.acc_Y = 0
+      self.acc_Z = 0
 
       frameSta = Frame(self)
       frameSta.pack(fill=X, anchor=W)
@@ -122,24 +86,24 @@ class MyFrame(Frame):
       lblBatt = Label(frameSta, text="Battery", width=10)
       lblBatt.pack(side=LEFT, padx=4, pady=8)
 
-      lblBattP = Label(frameSta, text="{0}%  |".format(battery_per))
-      lblBattP.pack(side=LEFT, padx=4, pady=8)
+      self.lblBattP = Label(frameSta, text="{0}%  |".format(self.battery_per))
+      self.lblBattP.pack(side=LEFT, padx=4, pady=8)
 
       #temperature
 
       lblTemp = Label(frameSta, text="Temperature", width=10)
       lblTemp.pack(side=LEFT, padx=4, pady=8)
 
-      lblTempC = Label(frameSta, text="{0}C  |".format(temp))
-      lblTempC.pack(side=LEFT, padx=4, pady=8)
+      self.lblTempC = Label(frameSta, text="{0}C  |".format(self.temp))
+      self.lblTempC.pack(side=LEFT, padx=4, pady=8)
 
       #pressure
 
       lblPres = Label(frameSta, text="Pressure", width=10)
       lblPres.pack(side=LEFT, padx=4, pady=8)
 
-      lblPresP = Label(frameSta, text="{0}".format(press))
-      lblPresP.pack(side=LEFT, padx=4, pady=8)
+      self.lblPresP = Label(frameSta, text="{0}".format(self.press))
+      self.lblPresP.pack(side=LEFT, padx=4, pady=8)
 
       #Attitude
       frameAtt = Frame(self)
@@ -148,9 +112,9 @@ class MyFrame(Frame):
       lblAtti = Label(frameAtt, text="Attitude", width=10)
       lblAtti.pack(padx=8, pady=8)
 
-      texAtti = Text(frameAtt, height=30, width=30, state=DISABLED)
-      texAtti.insert(1.0, "ROLL:{0} PITCH:{1} YAW:{2}\n".format(attitude_R, attitude_P, attitude_Y))
-      texAtti.pack(pady=10, padx=10)
+      self.texAtti = Text(frameAtt, height=30, width=30)
+      self.texAtti.insert(1.0, "ROLL:{0} PITCH:{1} YAW:{2}\n".format(self.attitude_R, self.attitude_P, self.attitude_Y))
+      self.texAtti.pack(pady=10, padx=10)
       
       #angle
       frameAng = Frame(self)
@@ -159,9 +123,9 @@ class MyFrame(Frame):
       lblAng = Label(frameAng, text="Angle", width=10)
       lblAng.pack(padx=8, pady=8)
 
-      texAng = Text(frameAng, height=30, width=30, state=DISABLED)
-      texAtti.insert(1.0, "ROLL:{0} PITCH:{1} YAW:{2}\n".format(angle_R, angle_P, angle_Y))
-      texAng.pack(pady=10, padx=10)
+      self.texAng = Text(frameAng, height=30, width=30)
+      self.texAng.insert(1.0, "ROLL:{0} PITCH:{1} YAW:{2}\n".format(self.angle_R, self.angle_P, self.angle_Y))
+      self.texAng.pack(pady=10, padx=10)
 
       #accel
       frameAcc = Frame(self)
@@ -170,9 +134,9 @@ class MyFrame(Frame):
       lblAcc = Label(frameAcc, text="Accel", width=10)
       lblAcc.pack(padx=8, pady=8)
 
-      texAcc = Text(frameAcc, height=30, width=30, state=DISABLED)
-      texAcc.insert(1.0, "X:{0} Y:{1} Z:{2}\n".format(acc_X, acc_Y, acc_Z))
-      texAcc.pack(pady=10, padx=10)
+      self.texAcc = Text(frameAcc, height=30, width=30)
+      self.texAcc.insert(1.0, "X:{0} Y:{1} Z:{2}\n".format(self.acc_X, self.acc_Y, self.acc_Z))
+      self.texAcc.pack(pady=10, padx=10)
 
       #plot_Check
       framePlo = Frame(self)
@@ -223,32 +187,57 @@ class MyFrame(Frame):
       button_RIGHT = Button(frameRI, width=5, height=2, text="→", command=moveRight)
       button_RIGHT.pack(anchor=NW)
 
-      def update_text(self):
-         if texAtti.see(100.0):
-            texAtti.delete(99.0,100.0)
-            texAng.delete(99.0,100.0)
-            texAcc.delete(99.0,100.0)
-         self.update()
-         self.after(1000, self.update_text)
+   def droneStat(self):
+      if drone.isConnected():
+         self.temp = drone.get_drone_temp()
+         self.press = drone.get_pressure()
+         self.battery_per = drone.get_battery_percentage()
+      self.after(1000, self.droneStat)
+
+   def droneAtti(self):
+      if drone.isConnected():
+         self.attitude_R = drone._data.attitude.ROLL
+         self.attitude_P = drone._data.attitude.PITCH
+         self.attitude_Y = drone._data.attitude.YAW
+      self.after(1000, self.droneAtti)
+
+   def droneAngl(self):
+      if drone.isConnected():
+         self.angle_R = drone._data.gyro.ROLL
+         self.angle_P = drone._data.gyro.PITCH
+         self.angle_Y = drone._data.gyro.YAW
+      self.after(1000, self.droneAngl)
+
+   def droneAcce(self):
+      if drone.isConnected():
+         self.acc_X = drone._data.accel.X
+         self.acc_Y = drone._data.accel.Y
+         self.acc_Z = drone._data.accel.Z
+      self.after(1000, self.droneAcce)
+
+   def update_text(self):
+      self.upBattP = "{0}%  |".format(self.battery_per)
+      self.upTempC = "{0}C  |".format(self.temp)
+      self.upPresP = "{0}".format(self.press)
+      self.lblBattP.configure(text=self.upBattP)
+      self.lblTempC.configure(text=self.upTempC)
+      self.lblPresP.configure(text=self.upPresP)
+      self.texAtti.insert(1.0, "ROLL:{0} PITCH:{1} YAW:{2}\n".format(self.attitude_R, self.attitude_P, self.attitude_Y))
+      self.texAng.insert(1.0, "ROLL:{0} PITCH:{1} YAW:{2}\n".format(self.angle_R, self.angle_P, self.angle_Y))
+      self.texAcc.insert(1.0, "X:{0} Y:{1} Z:{2}\n".format(self.acc_X, self.acc_Y, self.acc_Z))
+
+      self.after(1000, self.update_text)
 
 
 def main():
-   t1 = Thread(target=droneStat)
-   t2 = Thread(target=droneAtti)
-   t3 = Thread(target=droneAngl)
-   t4 = Thread(target=droneAcce)
-   t1.daemon = True
-   t2.daemon = True
-   t3.daemon = True
-   t4.daemon = True
-   t1.start()
-   t2.start()
-   t3.start()
-   t4.start()
-
    root = Tk()
    root.geometry("1000x450+100+100")
-   MyFrame(root)
+   root = MyFrame(root)
+   root.droneStat()
+   root.droneAtti()
+   root.droneAngl()
+   root.droneAcce()
+   root.update_text()
    root.mainloop()
 
 if __name__ == '__main__':
